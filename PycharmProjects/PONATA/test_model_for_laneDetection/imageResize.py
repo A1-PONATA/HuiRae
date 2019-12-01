aac2836f890b969e63d8830742a8e616c5277665
hayoung.is.here <hayoung.is.here@gmail.com>
2019-12-01 10:28:03 +0900

    change file path in code

--- PycharmProjects/PONATA/test_model_for_laneDetection/imageResize.py
+++ PycharmProjects/PONATA/test_model_for_laneDetection/imageResize.py
@@ -5,14 +5,21 @@ import os
 resize all image files to 64x64
 '''
 
-path = "/home/pirl/Desktop/lane-4-data"
-save_path = "/home/pirl/Desktop/lane-4-data-modified/"
+path = "/home/pirl/Documents/splited_action_data2"
+save_path = "/home/pirl/Documents/splited-action-data2-modified/"
 folder_list = os.listdir(path)
 
 for d in folder_list:
     if not os.path.exists(save_path+d+'/'):
         os.mkdir(save_path+d)
-    for file in os.listdir(path+'/'+d):
-        img = cv2.imread(path+'/'+d+'/'+file)
-        shrink = cv2.resize(img,(64,64), None, interpolation=cv2.INTER_AREA)
-        cv2.imwrite(save_path+d+'/'+file,shrink)
+    for dir in os.listdir(path+'/'+d):
+        #print(dir)
+        newPath = path+'/'+d+'/'+dir
+        for file in os.listdir(newPath):
+            filepath = newPath + "/"+file
+            #print(filepath)
+            img = cv2.imread(filepath, cv2.IMREAD_COLOR)
+            #print(img)
+            shrink = cv2.resize(img,(64,64), None, interpolation=cv2.INTER_AREA)
+            print(filepath)
+            cv2.imwrite(filepath, shrink)
